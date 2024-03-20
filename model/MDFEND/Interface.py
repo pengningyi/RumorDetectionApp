fc3d03e7e73456e8da71756ea46773fd6ed264b5
wuzelin <wuzelin@idea.edu.cn>
2024-03-20 11:53:20 +0800

    Add the MDFEND model

---
+++ model/MDFEND/Interface.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from models.mdfend import MultiDomainFENDModel
+from utils.dataloader import word2input
+
+import utils ##The local utils
+import sys
+import argparse
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+
+def main():
+
+
+    category_dict = {
+        "科技": 0,  
+        "军事": 1,  
+        "教育考试": 2,  
+        "灾难事故": 3,  
+        "政治": 4,  
+        "医药健康": 5,  
+        "财经商业": 6,  
+        "文体娱乐": 7,  
+        "社会生活": 8
+    }
+
+
+    config = {
+            'use_cuda': True,
+            'batchsize': 64,
+            'max_len': 170,
+            'early_stop': 3,
+            'num_workers': 4,
+            'vocab_file': './pretrained_model/chinese_roberta_wwm_base_ext_pytorch/vocab.txt',
+            'emb_type': 'bert',
+            'bert': './pretrained_model/chinese_roberta_wwm_base_ext_pytorch',
+            'root_path': './data/weibo21/',
+            'weight_decay': 5e-5,
+            'model':
+                {
+                'mlp': {'dims': [384], 'dropout': 0.2}
+                },
+            'emb_dim':768,
+            'lr': 0.0005,
+            'epoch': 50,
+            'model_name': 'mdfend',
+            'seed': 2021,
+            'save_param_dir': './param_model'
+            }
+    model = MultiDomainFENDModel(768,[384],'./pretrained_model/chinese_roberta_wwm_base_ext_pytorch',0.2,'bert')
+    model.load_state_dict(torch.load('/home/develop/workspace/MDFEND-Weibo21/param_model/mdfend/parameter_mdfend.pkl'))
+    model.cuda()
+
+
+    text = input("请输入文本：")
+
+    df = pd.DataFrame([{"content":"haha",'label':0,'category':"科技"},{"content":text,'label':1,'category':"科技"}])
+
+    content = df['content'].to_numpy()
+    label = torch.tensor(df['label'].astype(int).to_numpy())
+    category = torch.tensor(df['category'].apply(lambda c: category_dict[c]).to_numpy())
+    content_token_ids, content_masks = word2input(content, config['vocab_file'], config['max_len'])
+
+
+    data = {
+        'content': content_token_ids.cuda() ,
+        'content_masks': content_masks.cuda(),
+        'label': label.cuda(),
+        'category': category.cuda()
+        }
+
+    res = model(**data)
+
+    print(f"模型预测该文本为谣言的概率为: {res[1].detach().cpu().numpy()}")
+
+
+
+
+
+
+if __name__ =="__main__":
+    main()
---
+++ model/MDFEND/README.md
@@ -0,0 +1,34 @@
+# MDFEND: Multi-domain Fake News Detection
+This is an official implementation for [**MDFEND: Multi-domain Fake News Detection**](https://dl.acm.org/doi/abs/10.1145/3459637.3482139) which has been accepted by CIKM2021.
+## Dataset
+The splited dataset (i.e., train, val, test) are in the `MDFEND-Weibo21/data` folder.
+
+You can have access to the original dataset of Weibo21 only after an ["Application to Use Weibo21 for Fake News Detection"](https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAO__Q4mnQlURFcxUTBYOEZSWEk1SFA2Q1BRRDhaOTRQQi4u) has been submitted. 
+## Code
+### Requirements
+Refer to requirements.txt
+
+You can run `pip install -r requirements.txt` to deploy the environment quickly.
+### pretrained_model 
+You can download pretrained model (Roberta) from https://drive.google.com/drive/folders/1y2k22iMG1i1f302NLf-bj7UEe9zwTwLR?usp=sharing and move all the files in the folder into the path `MDFEND-Weibo21/pretrained_model/chinese_roberta_wwm_base_ext_pytorch`.
+### Data Preparation
+After you download the **Weibo21** dataset, move the `train.pkl`, `val.pkl` and `test.pkl` into the path `MDFEND-Weibo21/data`.
+### Run
+You can run this model through:
+```python
+python main.py --model_name mdfend --batchsize 32 --lr 0.0007
+```
+### Reference
+```
+Nan Q, Cao J, Zhu Y, et al. MDFEND: Multi-domain Fake News Detection[C]//Proceedings of the 30th ACM International Conference on Information & Knowledge Management. 2021: 3343-3347.
+```
+or in bibtex style:
+```
+@inproceedings{nan2021mdfend,
+  title={MDFEND: Multi-domain Fake News Detection},
+  author={Nan, Qiong and Cao, Juan and Zhu, Yongchun and Wang, Yanyan and Li, Jintao},
+  booktitle={Proceedings of the 30th ACM International Conference on Information \& Knowledge Management},
+  pages={3343--3347},
+  year={2021}
+}
+```
