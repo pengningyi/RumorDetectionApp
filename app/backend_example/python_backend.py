84e6233802e20d8acb3ca31e509a8ff8711e300a
pengningyi <137096822+pengningyi@users.noreply.github.com>
2024-04-15 19:14:29 +0800

    Update python_backend.py
    change routes for user&post

--- app/backend_example/python_backend.py
+++ app/backend_example/python_backend.py
@@ -11,11 +11,8 @@ def index():
     uid = '5623741644'##For example
     userInfo = userIndex.getUserInfo(uid)
     user_rumor_index = userIndex.get_user_index(uid)
-
-
-
+    
     text = '喝双氧水可以抗癌'## For example
-
     text_rumor_index = textIndex.get_text_index(text)
 
     return jsonify({'name' : userInfo['name'],
@@ -27,6 +24,36 @@ def index():
                     'test':text,
                     'text_rumor_index':text_rumor_index})
 
+@app.route('/user', methods = ['GET'])
+def index():
+
+    uid = '5623741644'##For example
+    userInfo = userIndex.getUserInfo(uid)
+    user_rumor_index = userIndex.get_user_index(uid)
+    
+    text = '喝双氧水可以抗癌'## For example
+    text_rumor_index = textIndex.get_text_index(text)
+
+    return jsonify({'name' : userInfo['name'],
+                    'id': userInfo['id'],
+                    'gender':userInfo['gender'],
+                    'follow_count':userInfo['follow_count'],
+                    'followers_count':userInfo['followers_count'],
+                    'user_rumor_index':user_rumor_index,})
+    
+@app.route('/post', methods = ['GET'])
+def index():
+
+    uid = '5623741644'##For example
+    userInfo = userIndex.getUserInfo(uid)
+    user_rumor_index = userIndex.get_user_index(uid)
+
+    text = '喝双氧水可以抗癌'## For example
+    text_rumor_index = textIndex.get_text_index(text)
+
+    return jsonify({'id': userInfo['id'],
+                    'test':text,
+                    'text_rumor_index':text_rumor_index})
 
 if __name__ == "__main__":
     app.run(host='119.29.91.23', port=80)
